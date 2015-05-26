20747436a8e154e00ac302a0bd72cc875e5be079
Masterlvng <masterlvng@gmail.com>
2015-05-26 11:14:13 +0800

    add prototype

--- conn.cpp
+++ conn.cpp
@@ -5,6 +5,40 @@
  *      Author: zhangliangfu
  */
 
+#include "conn.h"
 
+using namespace drift;
+
+Conn::Conn(int fd, Coro* c, Decoder* d): fd_(fd), coro_(c), d_(d)
+{
+	input_  = new Buffer(16 * 1024); //16K
+	output_ = new Buffer(16 * 1024); //16K
+}
+
+Conn::~Conn()
+{
+	delete input_;
+	delete output_;
+}
+
+int Conn::read()
+{
+	return 0;
+}
+
+int Conn::write()
+{
+	return 0;
+}
+
+int Conn::parse(std::string* ret)
+{
+	return 0;
+}
+
+int Conn::connect(char* ip, int port)
+{
+	return 0;
+}
 
 
--- conn.h
+++ conn.h
@@ -9,19 +9,29 @@
 #define CONN_H_
 
 #include "coro.h"
+#include "buffer.h"
+#include "message.h"
+
+#include <string>
 
 namespace drift
 {
 	class Conn
 	{
 	public:
+		Conn(int fd, Coro* c, Decoder* d);
+		~Conn();
 		int read();
 		int write();
+		int parse(std::string* ret); //parse input buffer. try to get complete message
+		int connect(char* ip, int port);
 
 	private:
-		int fd;
-		Coro* coro;
-
+		int fd_;
+		Coro* coro_;
+		Buffer* input_;
+		Buffer* output_;
+		Decoder* d_;
 	};
 };
 
